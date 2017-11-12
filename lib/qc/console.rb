0f435c4fa64178f409976ae624ecda1634a55cc6
Jorge Manrubia <jorge.manrubia@gmail.com>
2017-11-12 11:50:32 +0100

    Add basic help and version options

--- lib/qc/console.rb
+++ lib/qc/console.rb
@@ -4,6 +4,8 @@ module Qc
     end
 
     def run(argv)
+      options = parse_options(argv)
+
       command = argv[0]
       client = Qc::CommandRunner.new(quant_connect_proxy)
       success = begin
@@ -24,6 +26,33 @@ module Qc
 
     def quant_connect_proxy
       @quant_connect_proxy ||= Qc::QuantConnectProxy.new(Qc::Credentials.read_from_home)
+    end
+
+    def parse_options(args)
+      options = OpenStruct.new
+
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = "Usage: qc [command] [options]"
+
+        opts.separator ""
+        opts.separator "Supported commands: #{Qc::CommandRunner::SUPPORTED_COMMANDS.join(', ')}"
+
+        opts.separator ""
+        opts.separator "Common options:"
+
+        opts.on("-h", "--help", "Show this message") do
+          puts opts
+          exit 0
+        end
+
+        opts.on("--version", "Show version") do
+          puts Qc::VERSION
+          exit 0
+        end
+      end
+
+      opt_parser.parse!(args)
+      options
     end
   end
 end
